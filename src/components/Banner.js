a232019019a33e97f92fbbd7911a17c4a97e3df7
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-08-06 17:27:20 +0530

    updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../Assets/img/-1_orig.jpeg";
+import headerImg from "../Assets/img/Me.png";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
