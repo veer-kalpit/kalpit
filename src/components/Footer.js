ef78fd4d7e963813810caa8a57392a30d085c9e3
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-08-06 15:58:20 +0530

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
+          <Col size={12} sm={10}>
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
