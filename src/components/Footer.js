07f495c0556dc5a68661d7cc6131e5eb86260c0f
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-08-10 22:35:07 +0530

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,6 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/kalpit-thakur-9158a7242/">
