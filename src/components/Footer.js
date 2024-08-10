ed68aa7145d35e682f14d3f5597329bd1f5d46d6
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-08-10 22:54:39 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,10 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={20} sm={6} className="text-center text-sm-end">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/kalpit-thakur-9158a7242/">
                 <img src={navIcon1} alt=""></img>
